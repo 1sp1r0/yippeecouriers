04d0dd39820e38067032f90fe431e49324424572
David Ordal <david@ordal.com>
2016-02-27 14:20:13 -0800

    corrected error on the estimate model

--- app/models/estimate.js
+++ app/models/estimate.js
@@ -4,37 +4,39 @@ var Schema = mongoose.Schema;
 // create a schema for Pet
 var estimateSchema = new Schema({
     departure_date: { type: Date, required: true },
-    return_date:  { type: Date, required: true },	
+    return_date:  { type: Date, required: true },   
     flight: { 
-    	cost_range: {
-    		low: { type: Number, required: true },
-    		high: { type: Number, required: true },
-    	},
-    	orig_code: { type: String, required: true },
-    	orig_coordinates: {
-    		latitude: { type: String, required: true },
-    		longitude: { type: String, required: true },
-    	},
-    	dest_code: { type: String, required: true },
-    	dest_coordinates: {
-    		latitude: { type: String, required: true },
-    		longitude: { type: String, required: true },
-    	},
+        cost_range: {
+            low: { type: Number, required: true },
+            high: { type: Number, required: true },
+        },
+        orig_code: { type: String, required: true },
+        orig_coordinates: {
+            latitude: { type: Number, required: true },
+            longitude: { type: Number, required: true },
+        },
+        dest_code: { type: String, required: true },
+        dest_coordinates: {
+            latitude: { type: Number, required: true },
+            longitude: { type: Number, required: true },
+        },
     },
     hotel: {
-    	cost_range: {
-    		low: { type: Number, required: true },
-    		high: { type: Number, required: true },
-    	},
+        cost_range: {
+            low: { type: Number, required: true },
+            high: { type: Number, required: true },
+        },
     },
     airline_pet_fee:  { type: Number, required: true },
     yippee_fee:  { type: Number, required: true },
     misc_fee: { type: Number, required: false },
-    total_fee: { type: Number, required: true },
+    total_fee: {
+        low: { type: Number, required: true },
+        high: { type: Number, required: true },
+    },
     created_at: Date,
     updated_at: Date
 });
 
-
-var Pet = mongoose.model('Estimate', estimateSchema);
+var Estimate = mongoose.model('Estimate', estimateSchema);
 module.exports = Estimate;
