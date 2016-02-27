97fa3ee578765901ad2f030075a3bf893d5bf007
David Ordal <david@ordal.com>
2016-02-27 13:06:52 -0800

    updated data models

--- app/models/pet.js
+++ app/models/pet.js
@@ -3,8 +3,13 @@ var Schema = mongoose.Schema;
 
 // create a schema for Pet
 var petSchema = new Schema({
-    name: { type: String, required: false, unique: false }, //sender or receiver
-    weight: { type: Number, required: false, unique: false },
+    name: { type: String, required: true },
+    image_url: { type: String, required: false },
+    species: { type: String, required: true },
+    weight: { type: Number, required: true },
+    age: {type: Number, required: true },
+    medical_notes: {type: Number, required: true },
+    has_carrier: {type: Boolean, required: true },
     _trip: {type: mongoose.Schema.Types.ObjectId, ref:'Trip', required: false },
     created_at: Date,
     updated_at: Date
--- app/models/trip.js
+++ app/models/trip.js
@@ -3,15 +3,34 @@ var Schema = mongoose.Schema;
 
 // create a schema for Trip
 var tripSchema = new Schema({
-    trip_name: { type: String, required: false, unique: false }, //sender or receiver
-    main_contact: { type: String, required: false, unique: false }, //sender or receiver
-    sender_name: { type: String, required: false, unique: false },
-    sender_email: { type: String, required: false, unique: false },
-    sender_phone: { type: String, required: false, unique: false },
-    receiver_name: { type: String, required: false, unique: false },
-    receiver_email: { type: String, required: false, unique: false },
-    receiver_phone: { type: String, required: false, unique: false },
-    _pets: [{type: mongoose.Schema.Types.ObjectId, ref:'Pet', required: false }],
+    trip_name: { type: String, required: false },    // friendly name; 
+    status: {type: String, required: true},
+    main_contact: { type: String, required: false }, //sender or receiver
+    sender_name: { type: String, required: true },
+    sender_email: { type: String, required: true },
+    sender_phone: { type: String, required: true },
+    receiver_name: { type: String, required: true },
+    receiver_email: { type: String, required: true },
+    receiver_phone: { type: String, required: true },
+    pickup_date: {type: Date, required: true},
+    pickup_address: {
+        address1: { type: String, required: true },
+        address2: { type: String, required: false },
+        city: { type: String, required: true },
+        state: { type: String, required: true },
+        postcode: { type: String, required: true },
+    },
+    dropoff_date: {type: Date, required: true},
+    dropoff_address: {
+        address1: { type: String, required: true },
+        address2: { type: String, required: false },
+        city: { type: String, required: true },
+        state: { type: String, required: true },
+        postcode: { type: String, required: true },
+    },
+    trip_notes: {type: String, required: false},
+    _pets: [{type: mongoose.Schema.Types.ObjectId, ref:'Pet', required: true }],
+    _estimateID: {type: mongoose.Schema.Types.ObjectId, ref:'Estimate', required: false},
     created_at: Date,
     updated_at: Date
 });
