96cc30cc2789698898e1f234829e85201e974cac
ericboromisa <boromisa@gmail.com>
2016-02-26 16:17:17 -0800

    Express Server

--- server.js
+++ server.js
@@ -1,45 +1,10 @@
-//Lets require/import the HTTP module
-var http = require('http');
-//And Dispatcher
-var dispatcher = require('httpdispatcher');
+var express = require('express');
+var app = express();
 
-
-//Lets define a port we want to listen to
-const PORT=8080; 
-
-
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Start the server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-//Dispatcher defined
-function handleRequest(request, response){
-    try {
-        //log the request on console
-        console.log(request.url);
-        //Disptach
-        dispatcher.dispatch(request, response);
-    } catch(err) {
-        console.log(err);
-    }
-}
-
-//For all your static (js/css/images/etc.) set the directory name (relative path).
-dispatcher.setStatic('static');
-
-//A sample GET request    
-dispatcher.onGet("/", function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Page One');
-});    
-
-//A sample POST request
-dispatcher.onPost("/post1", function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Got Post Data');
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
