d640df964d7d9f291b7887562894366e505961c1
Jordan Hart <jordan@jordanhart.com>
2016-02-26 16:42:06 -0800

    installed modules, configd in app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+//Module Includes
+var express = require('express'),
+    app = express(),
+    router = express.Router(),
+    path = require('path'),
+    bodyParser = require('body-parser');
+
+//Database Config
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/yippee');
+
+//View Engine Config
+app.set('views', path.join(__dirname, 'app/views'));
+app.set('view engine', 'jade');
+
+//Body Parser Config
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( {
+    extended: true
+}));
+
+//Public Folder Config
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Controllers
+
+
+//Routes
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+//Server Launch
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
