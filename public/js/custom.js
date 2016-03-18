27aa6fd322601568d07df311e6f103dc78499423
David Ordal <david@ordal.com>
2016-03-17 20:26:36 -0700

    basic JS bootstrap and GA installed

--- public/js/custom.js
+++ public/js/custom.js
@@ -126,3 +126,20 @@ function requestTrip(){
         console.log('done ran');
     });
 }
+
+function pageBootstrap() {
+
+    // load Google Analytics
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-75289755-1', 'auto');
+    ga('send', 'pageview');
+
+    // create datepicker
+    $('#dp').datepicker({format: 'yyyy-mm-dd'});
+}
+
+
